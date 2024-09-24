f7cee898cd3613831e54ab6e120fce09ff6f7c57
Eivind Teig <eivind.teig@gmail.com>
2024-09-24 20:25:07 +0200

    Add verbs to filter.

--- endpoint_filter/main.py
+++ endpoint_filter/main.py
@@ -7,6 +7,7 @@ class EndpointFilter(logging.Filter):
         self,
         path: str,
         *args: Any,
+        verb: str = "GET",
         **kwargs: Any,
     ):
         """
@@ -19,14 +20,20 @@ class EndpointFilter(logging.Filter):
         Example:
         uvicorn_logger = logging.getLogger("uvicorn.access")
         uvicorn_logger.addFilter(EndpointFilter(path="/live"))
+        uvicorn_logger.addFilter(EndpointFilter(path="/live", verb="POST"))
         uvicorn_logger.addFilter(EndpointFilter(path="/endpoint"))
 
         :param path: The endpoint path to filter.
         :param args: Additional positional arguments.
+        :param verb: The HTTP verb to filter, default is "GET".
         :param kwargs: Additional keyword arguments.
         """
         super().__init__(*args, **kwargs)
         self._path = path
+        self._verb = verb
 
     def filter(self, record: logging.LogRecord) -> bool:
+        host, verb, path, version, status_code, *_ = record.args
+        if verb != self._verb:
+            return True
         return record.getMessage().find(self._path) == -1
